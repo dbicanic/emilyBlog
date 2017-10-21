27d1c34f4d5c00448fa12170b3de03446e4771bf
dbicanic <dbicanic77@gmail.com>
2017-10-20 21:21:08 -0500

    Have create admin started, just need to handle results

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,25 @@
 class AdminController < ApplicationController
-  def new
-  end
+
+	def show
+		@admin = Admin.find(params[:id])
+	end
+
+	def new
+		@admin = Admin.new
+	end
+
+	def create
+		@admin = Admin.new(adminParams)
+		if @admin.save
+
+		else
+			render "new"
+		end
+	end
+
+	private
+
+	def adminParams
+		params.require(:admin).permit(:name, :email, :password, :password_confirmation)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'admin#new'
-
-  get 'welcome/index'
+  get 'articles/index'
 
   resources :articles do
   	resources :comments
   end
 
-  root 'welcome#index'
+  get '/createAdmin', to: 'admin#new'
+  resources :admin
+
 end
