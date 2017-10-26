74f22087de0ac305ff152e32bbbf6e3e70e54185
dbicanic <dbicanic77@gmail.com>
2017-10-25 22:16:09 -0500

    Start on login and sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		admin = Admin.find_by(email: params[:session][:email].downcase)
+		if admin && admin.authenticate(params[:session][:password])
+			log_in admin
+			redirect_to admin
+		else
+			flash.now[:danger] = 'Invalid email/password combination'
+			render "new"
+		end
+	end
+
+	def destroy
+		
+	end
+end
